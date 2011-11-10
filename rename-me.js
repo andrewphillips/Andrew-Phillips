b4a35e5d7a641859a7e3273385bad025c8c45017
Andrew Phillips <andrewtylerphillips@gmail.com>
2011-11-10 10:06:33 -0500

    Beginning with last weeks' story - bringing in some code/tweaking it.

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,26 @@
-alert("JavaScript works!");
+//Andrew Phillips
+// November 10 2011
+// Project 3
+// Apps & Cases
+
+var appName = "CRAZY SUPER MONSTER SLAYER on SUNDAY";
+var omgApps = [ "Puzzle", "RPG", "Family"];
+var omgHowMany = [ 475, 1000, 973];
+
+
+// Method Procedure _I_
+
+
+var adventureStart = function(lookInAdventure) {
+	console.log("I think this game is in the Adventure genre.");
+	if(lookInAdventure >= 8) {
+		console.log("It doesn\'t seem to be an Adventure game.  Dangit!");
+	} else {
+		console.log("Yeah it\'s got to be an adventure game!  Cool beans!");
+			if(lookInAdventure >= 5 && lookInAdventure <= 12) {
+				console.log("Wait - who says \'Cool Beans\' anymore?");
+			} else {
+				console.log("Saying \'Cool Beans\' makes me awesome!");
+			};
+	};
+};
