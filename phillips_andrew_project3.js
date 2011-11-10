8ba55c487fb3c50ace4fb095a664cb6af982bdea
Andrew Phillips <andrewtylerphillips@gmail.com>
2011-11-10 10:36:28 -0500

    last week pt 2. - Method Function start

--- phillips_andrew_project3.js
+++ phillips_andrew_project3.js
@@ -12,18 +12,38 @@ var omgHowMany = [ 475, 1000, 973];
 var	lookedAdventure = {
 		type: "game",
 		categories: 19,
-			adventureStart = function (search) {
+			adventureStart: function (search) {
 			console.log("I think this " + this.type + " is in the " + search + " genre.");
-			if(this.categories >= 8) {
-				console.log("It doesn\'t seem to be an Adventure game.  Dangit!");
+			if (this.categories >= 20) {
+				console.log("It doesn\'t seem to be an " + search + " game.  Dangit!");
 			} else {
-				console.log("Yeah it\'s got to be an adventure game!  Cool beans!");
-					if(lookInAdventure >= 5 && lookInAdventure <= 12) {
+				console.log("Yeah it\'s got to be an " + search + " game!  Cool beans!");
+					if(this.categories >= 10 && this.categories <= 20) {
 						console.log("Wait - who says \'Cool Beans\' anymore?");
 					} else {
 						console.log("Saying \'Cool Beans\' makes me awesome!");
-			};
-	};
+			}
+		}	
+	}
 };
 
 lookedAdventure.adventureStart("Adventure");
+
+
+// _II_
+
+var scrollKing = function ( needRPG, needAction ) {
+	var notAdventure;
+	if ( needRPG <= 20 ) {
+		if( needAction === true) {
+			console.log("I guess it\'s not an Adventure game.  I should look in the Action genre, or possibly others.");
+		} else {
+			console.log("I found the game!  That was quick!");
+		};
+	} else {
+		console.log("The game has nothing to do with adventure.  Why am I looking here?");
+	};
+
+	notAdventure = "Oh wow.  There are " + needRPG + " genres on the AppStore.";
+	return notAdventure;
+};
